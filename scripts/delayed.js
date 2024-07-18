1cade778a5445470b5bae40d928221c980c30419
Laurel Timko <laurel.timko@gmail.com>
2024-07-18 16:36:56 -0400

    Initial GTM Code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,38 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './aem.js';
 
+// google tag manager
+function loadGTM() {
+  //ContainerID GTM-NGNNXZW
+  const scriptTag = document.createElement('script');
+  scriptTag.innerHTML = `
+  // googleTagManager
+  (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+          'gtm.start':
+              new Date().getTime(), event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-NGNNXZW');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('set', {
+      'cookie_flags': 'SameSite=None;Secure'
+  });
+  `;
+  document.head.prepend(scriptTag);
+}
+
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-// add more delayed functionality here
+console.log('here in delayed');
+if (!window.location.hostname.includes('localhost') && !document.location.hostname.includes('.hlx.page')) {
+  loadGTM();
+}
